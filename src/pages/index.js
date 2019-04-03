307c7e24653cc68f703e4e494d949d3c090a9fdd
qiaoyu <rainoy.me@gmail.com>
2019-04-03 19:17:54 +0800

    首页

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,16 +6,24 @@ import Image from '../components/image';
 import SEO from '../components/seo';
 import Slide from '../components/slide';
 import DesBar from '../components/desbar';
+import SectionVideo from '../components/SectionVideo';
+import SectionBytom from '../components/SectionBytom';
+import SectionDev from '../components/SectionDev';
+import Trends from '../components/Trends';
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+    <SEO title="Home" keywords={[`bytom`, `btm`, `比原`, `比原链`]} />
     <Slide></Slide>
     <DesBar />
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+    <SectionVideo></SectionVideo>
+    <SectionBytom></SectionBytom>
+    <SectionDev></SectionDev>
+    {/* <Trends></Trends> */}
+    {/* <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Link to="/page-2/">Go to page 2</Link> */}
   </Layout>
 )
 
