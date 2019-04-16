b54133bd17d01646fc3a43a570ca5f920b0b4f33
qiaoyu <rainoy.me@gmail.com>
2019-04-16 19:20:40 +0800

    文案更新

--- src/conf/home.conf.js
+++ src/conf/home.conf.js
@@ -1,4 +1,5 @@
 import img_banner from '../images/gatsby-icon.png';
+import img_icon_des from '../images/icon-des.png';
 
 
 export default {
@@ -10,20 +11,41 @@ export default {
   ],
   features: [
     {
-      img: img_banner,
+      img: img_icon_des,
       title: 'Intelligent',
       cont: 'Consensus algorithm promotes AI techniques, calculation power actually using.'
     },
     {
-      img: img_banner,
+      img: img_icon_des,
       title: 'Flexible',
       cont: 'Customize Your Smart contract, completely control assets, power the future of the econmy.'
     },
     {
-      img: img_banner,
+      img: img_icon_des,
       title: 'Efficient',
       cont: 'Digital Asset Atomic Transfer, high concurrent exchange, adaptable to different business environments.'
     },
-  ]
+  ],
+  videoInfo: {
+    title: 'WHY',
+    subTitle: 'BYTOM',
+    desc: 'Bytom Blockchain Protocol is an interactive protocol of multiple byteassets. Heterogeneous byte-assets that operate in different forms on the Bytom Blockchain.',
+    list: [
+      'Compatible with the UTXO',
+      'General address format',
+      'Asset naming using ODIN',
+      'Enhanced trading flexibility',
+      'Cross-chain & Side-chain',
+      'Friendly to AI ASIC-chips',
+      'Separate transaction signatures',
+      'Encryption standard compatible',
+    ]
+  },
+  bytomInfo: {
+    title: 'HIERARCHICAL',
+    titleDes: 'OF BYTOM',
+    subTitle: 'MODEL',
+    desc: 'Bytom adopts three-layer architecture which is friendly to develop asset management applications, issue and manage assets as well as spend, transfer and other operations through  application layer, contract layer and data transmission layer.Bytom’s consensus mechanism is POW algorithm which is friendly to Al ASIC chips.'
+  }
 
 };
